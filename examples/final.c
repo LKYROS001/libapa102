faaefcc523dfd12a2dcf543e32259bb8e3c6aedb
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-10-18 16:39:04 +0200

    Update final.c

--- examples/final.c
+++ examples/final.c
@@ -3,23 +3,25 @@
 
 int main() {
   struct APA102* strip = APA102_Init(60);
+  struct APA102_Frame* color = APA102_CreateFrame(10, 255, 255, 0);
+  struct APA102_Frame* color2 = APA102_CreateFrame(31, 0, 255, 255);
   int x;
   for(x = 0; x < 1000; i++) {
     APA102_Begin();
     int i;
     for(i = 0; i < 60; i++) {
-      APA102_WriteLED(strip, APA102_CreateFrame(10, 255, 255, 0));
+      APA102_WriteLED(color);
     }
     APA102_End();
-    delay(5);
+    delay(1000);
     
     APA102_Begin();
     int i;
     for(i = 0; i < 60; i++) {
-      APA102_WriteLED(strip, APA102_CreateFrame(10, 0, 255, 255));
+      APA102_WriteLED(color2);
     }
     APA102_End();
-    delay(5)
+    delay(1000)
   }
   return 0;
 }
