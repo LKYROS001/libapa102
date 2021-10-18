af73f44d9ab4cecd187e91a0aea58265cd2cc7f2
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-10-18 16:04:55 +0200

    Create preview.c

---
+++ examples/preview.c
@@ -0,0 +1,13 @@
+#include <apa102.h>
+
+int main() {
+  struct APA102* strip = APA102_Init(60);
+  APA102_Begin();
+  int i;
+  for(i = 0; i < 60; i++) {
+    APA102_WriteLED(strip, APA102_CreateFrame(31, 0xFF, 0xFF, 0x00));
+  }
+  APA102_End();
+  
+  return 0;
+}
