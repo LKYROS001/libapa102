0a6ecec2adf4f96fe42c07f5299c68d790f265e6
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-10-18 16:32:06 +0200

    Update preview.c

--- examples/preview.c
+++ examples/preview.c
@@ -3,10 +3,11 @@
 
 int main() {
   struct APA102* strip = APA102_Init(60);
+  struct APA102_Frame* color = APA102_CreateFrame(31, 255, 255, 0);
   APA102_Begin();
   int i;
   for(i = 0; i < 60; i++) {
-    APA102_WriteLED(strip, APA102_CreateFrame(31, 0xFF, 0xFF, 0x00));
+    APA102_WriteLED(color);
   }
   APA102_End();
   
