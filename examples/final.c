85dcedeaf8a2a7ae1416e6c624b2ce882f03db4c
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-10-18 17:06:45 +0200

    Update final.c

--- examples/final.c
+++ examples/final.c
@@ -1,14 +1,19 @@
 #include <apa102.h>
 #include <wiringPi.h>
-
+#include <stdio.h>
 
 int main() {
   struct APA102* strip = APA102_Init(60);
   struct APA102_Frame* color = APA102_CreateFrame(1, 255, 255, 0);
   struct APA102_Frame* color2 = APA102_CreateFrame(1, 0, 255, 255);
   int x;
+  int z;
   for(x = 0; x < 10000000; x++) {
-  
+    int z++;
+    if (z==360){
+      printf("YESSS \n");
+      z=0;
+    }
     APA102_Begin();
     int i;
     for(i = 0; i < 60; i++) {
