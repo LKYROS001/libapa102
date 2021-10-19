12faa06257e1a9ea86f8be1d38e4842120f9adbc
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-10-19 14:06:06 +0200

    Update multistripes.c

--- examples/multistripes.c
+++ examples/multistripes.c
@@ -35,7 +35,7 @@ int main() {
       printf("YESSS \n");
       z=0;
     }
-    APA102_MultiStripes(strip, colors, 4, 2, 0, 0);
+    APA102_MultiStripes(strip, colors,  60, 0, 0, 0);
     //APA102_MultiStripes(strip, colors2, 4, 2, 0, 0);
   }
 }
